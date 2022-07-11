6a4a64b81e93f34aea95453524ce61c0a19d1d66
Mayur7755 <42093688+Mayur7755@users.noreply.github.com>
2022-07-11 23:55:30 +0800

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,3 +1,10 @@
-#!/bin/bash
-
-echo "Hello World!"
+pipeline {
+    agent { docker { image 'maven:3.8.4-openjdk-11-slim' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'mvn --version'
+            }
+        }
+    }
+}
