f472d87e03f6f826f501b65956b2f4081a5b50f7
Mayur7755 <42093688+Mayur7755@users.noreply.github.com>
2022-07-12 01:36:07 +0800

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -46,11 +46,55 @@ pipeline {
                 echo "Hello World"
             }
         }
+	    
+	 stage("Fortify Scan"){
+		steps{
+			
+			echo "Fortify scan successfull"
+			        
+		}
+			
+			
+	}
         stage('Quality_gate') {
             steps {
                 echo "quality gate is successful"
             }
         }
+	 stage('Upload Artifact to Nexus'){
+		steps{
+			        
+			echo " Upload Artifact to Nexus is successfull"   
+		}
+	}   
+	 stage('Deploy-L1'){  
+		steps{
+			
+			echo "Deploy-L1 is successfull"
+			        
+		}
+			
+			
+	}
+	stage('L1-Validate Env'){ 
+		steps{
+			
+			echo "L1-Validate Env is successfull"
+			        
+		}
+			
+		
+	}
+	stage('L1-RegressionTest'){
+		steps{
+			
+			echo "L1-RegressionTest is successfull"
+			        
+		}
+			
+			
+	}    
+	    
         
     }
 }
